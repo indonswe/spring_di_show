1523e3c252ea4362a31a62399b1bd0351239285f
indonswe <afc2002afc@hotmail.com>
2021-02-24 22:09:50 +0100

    First commit spring

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        //commenting
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
